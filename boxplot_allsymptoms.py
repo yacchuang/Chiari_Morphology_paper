effb524b89c4fce32cb0d4913f8132acbb448b06
yacchuang <99505282+yacchuang@users.noreply.github.com>
2022-03-29 18:16:31 -0700

    update symptoms morpho graphs

--- boxplot_allsymptoms.py
+++ boxplot_allsymptoms.py
@@ -16,7 +16,7 @@ from xlwt import Workbook
 
 
 ### load data
-df =  pd.read_excel("/Users/ya-chenchuang/Desktop/Stevens/projects/Morphology/results/symptoms_correlation.xlsx", sheet_name='allsymptoms_2D', header = None, index_col = None);
+df =  pd.read_excel("/Users/kurtlab/Desktop/Chiari_Morphometric/results/symptoms_morph/symptoms_correlation.xlsx", sheet_name='symptoms_new2', header = None, index_col = None);
 print(df.shape) 
 print("Column headings:")
 print(df.columns)
@@ -25,10 +25,10 @@ print(df.columns)
 wb = Workbook()
 
 # add_sheet is used to create sheet.
-sheet1 = wb.add_sheet('TonsilL')
+sheet1 = wb.add_sheet('FMaRatio')
 
 col = 0
-for symptoms in range(2,17):
+for symptoms in range(2,18):
     sheet1.write(0, col, df.loc[0, symptoms])
     col = col + 1
 
@@ -36,7 +36,7 @@ for symptoms in range(2,17):
 ### classify symptoms data
 # read morphometric data:
 # set Y = 1; N = 0
-for column in df[2:16]:
+for column in df[2:18]:
     df.loc[df[column]=="N", column]=0
     df.loc[df[column]=="Y", column]=1
     
@@ -49,14 +49,14 @@ col = 0
 for column in range(2,17):
     for i in df.loc[:, column]:
         if i == 1:
-            sheet1.write(row, col, df.iloc[row, 19])
+            sheet1.write(row, col, df.iloc[row, 24])
             # print(df.iloc[row, 19])
         row = row + 1
     row = 0
     col = col + 1
 
 # write the data in a new excel sheet 
-wb.save('/Users/ya-chenchuang/Desktop/Stevens/projects/Morphology/results/symptoms_TonsilL.xls')
+wb.save('/Users/kurtlab/Desktop/Chiari_Morphometric/results/symp_FMaRatio.xls')
 
 '''
 ### make boxplot 
--- symptom_morphology_boxplot.py
+++ symptom_morphology_boxplot.py
@@ -8,9 +8,16 @@ Created on Thu Feb 17 15:56:46 2022
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+
+# set the font globally
+plt.rcParams.update({'font.family':'CMR17'})
 
 
-SymptomData = pd.read_excel('/Users/kurtlab/Desktop/Chiari Morphometric/results/symptoms_morph/symptoms_BSv.xls', sheet_name='BSv')
+SymptomData = pd.read_excel('/Users/kurtlab/Desktop/Chiari_Morphometric/results/symptoms_morph/symptoms_TonsilV.xls', sheet_name='Sheet1')
 
 Tonsilboxplot = SymptomData.boxplot(fontsize=None, rot=90)
-Tonsilboxplot.set_ylabel("Brainstem volume (mm^3)", fontsize = 14) 
+Tonsilboxplot.set_ylabel("Tonsil volume (mm^3)", fontsize = 14) 
+
+plt.show()
+
