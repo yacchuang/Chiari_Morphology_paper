193064e4d6b95fcd0735fc13349cdb39fa5f07b6
yacchuang <99505282+yacchuang@users.noreply.github.com>
2022-04-01 11:29:18 -0700

    angle boxplot

--- boxplotpractice.py
+++ boxplotpractice.py
@@ -12,9 +12,9 @@ from scipy import stats
 import numpy as np
 from scipy.stats import mannwhitneyu, normaltest
 
-dfHealthy = pd.read_excel("/Users/ya-chenchuang/Desktop/Stevens/projects/Morphology/results/morphometric_stat_combine.xlsx", sheet_name='HealthyVolume');
-dfChiari = pd.read_excel("/Users/ya-chenchuang/Desktop/Stevens/projects/Morphology/results/morphometric_stat_combine.xlsx", sheet_name='ChiariVolume');
-VariableName = "4thVentricle"
+dfHealthy = pd.read_excel("/Users/kurtlab/Desktop/Chiari_Morphometric/results/morphology_results/morphometric_stat_combine.xlsx", sheet_name='HealthyAngle');
+dfChiari = pd.read_excel("/Users/kurtlab/Desktop/Chiari_Morphometric/results/morphology_results/morphometric_stat_combine.xlsx", sheet_name='ChiariAngle');
+VariableName = "Clivus_canal"
 
 HealthyVolume = pd.DataFrame(dfHealthy[VariableName].values, index = None, columns = [VariableName]); 
 HealthyVolume['HealthyorChiari'] = "Healthy";
@@ -26,8 +26,8 @@ ChiariVolume['HealthyorChiari'] = "Chiari";
 Compare = HealthyVolume.append(ChiariVolume);
 
 # statistics
-Healthy = dfHealthy['4thVentricle']
-Chiari = dfChiari['4thVentricle']
+Healthy = dfHealthy['Clivus_canal']
+Chiari = dfChiari['Clivus_canal']
 
 log_Healthy = np.log(Healthy)
 log_Chiari = np.log(Chiari)
@@ -39,9 +39,9 @@ print("Healthy vs Chiari: \n", stat_results, "\n")
 
 # statistical annotation
 x1, x2 = 0, 1   # columns 'Sat' and 'Sun' (first column: 0, see plt.xticks())
-y, h, col = dfHealthy['4thVentricle'].max() + 150, .1, 'k'
+y, h, col = dfChiari['Clivus_canal'].max() + 1, .1, 'k'
 plt.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=1.5, c=col)
-plt.text((x1+x2)*.5, y+h, "****", ha='center', va='bottom', color=col)
+plt.text((x1+x2)*.5, y+h, "ns", ha='center', va='bottom', color=col)
 
 
 # df = sns.load_dataset('test')
@@ -49,7 +49,7 @@ plt.text((x1+x2)*.5, y+h, "****", ha='center', va='bottom', color=col)
 
 sns.set()
 plot = sns.boxplot(x='HealthyorChiari', y = VariableName, data = Compare);
-plot.set_ylabel("4th Ventricle volume (mm^3)", fontsize = 20)
+plot.set_ylabel("Clivus Canal angle", fontsize = 20)
 plt.xticks(fontsize=20)
 plt.yticks(fontsize=14)
 plt.show()
