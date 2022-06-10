3292b4850bf257104748b3a1e9a137c22b92be7a
yacchuang <99505282+yacchuang@users.noreply.github.com>
2022-06-10 11:26:27 -0700

    add ROC for syrinx

--- logistic_ROC_wholedata.py
+++ logistic_ROC_wholedata.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Feb 14 16:28:02 2022
+Created on Fri Jun 10 10:50:57 2022
 
 @author: ya-chenchuang
 """
@@ -35,7 +35,7 @@ df = df.dropna()
 
 ## Normalized Input
 # feature_names = ["TonsilL", "ratio", "ventricle", "TonsilV", "CBLv", "BSv", "Boogard", "Occipital"]
-feature_names = ["TonsilV", "CBLv"]
+feature_names = ["BS V", "CBL V"]
 for feature_name in feature_names:
     df[feature_name] = df[feature_name] / df[feature_name].std()
     
@@ -43,7 +43,7 @@ X = df[feature_names].values
 
 
 ## label symptoms
-label = df["condition"]
+label = df["syringomyelia"]
 # label = df["syringomyelia"]
 from sklearn.preprocessing import LabelEncoder 
 ly = LabelEncoder()
@@ -51,26 +51,21 @@ y = ly.fit_transform(label)
 
 
 
-## Splitting Data using Sklearn
-from sklearn.model_selection import train_test_split
-trainX,testX,trainy,testy = train_test_split(X,y,test_size=0.5, random_state=2)
-
-
 ## Logistic Regression using Sklearn
 from sklearn.linear_model import LogisticRegression
 logreg = LogisticRegression(solver = 'lbfgs',multi_class='auto', max_iter=100, C=1)
-logreg.fit(trainX,trainy)
+logreg.fit(X,y)
 
 
 # generate a no skill prediction (majority class)
-ns_probs = [0 for _ in range(len(testy))]
+ns_probs = [0 for _ in range(len(y))]
 
 ## Predict probabilities
-lr_probs = logreg.predict_proba(testX)
+lr_probs = logreg.predict_proba(X)
 # keep probabilities for the positive outcome only
 lr_probs = lr_probs[:, 1]
-score1 = logreg.score(trainX,trainy)
-y_pred1 = logreg.predict(testX)
+score1 = logreg.score(X,y)
+y_pred1 = logreg.predict(X)
 
 
 
@@ -87,14 +82,14 @@ print(equation)
 
 
 # calculate scores
-ns_auc = roc_auc_score(testy, ns_probs)
-lr_auc = roc_auc_score(testy, lr_probs)
+ns_auc = roc_auc_score(y, ns_probs)
+lr_auc = roc_auc_score(y, lr_probs)
 # summarize scores
 print('No Skill: ROC AUC=%.3f' % (ns_auc))
 print('Logistic: ROC AUC=%.3f' % (lr_auc))
 # calculate roc curves
-ns_fpr, ns_tpr, _ = roc_curve(testy, ns_probs)
-lr_fpr, lr_tpr, _ = roc_curve(testy, lr_probs)
+ns_fpr, ns_tpr, _ = roc_curve(y, ns_probs)
+lr_fpr, lr_tpr, _ = roc_curve(y, lr_probs)
 # plot the roc curve for the model
 plt.plot(ns_fpr, ns_tpr, linestyle='--', label='No Skill')
 plt.plot(lr_fpr, lr_tpr, marker='.', label='Logistic')
@@ -105,5 +100,7 @@ plt.ylabel('True Positive Rate')
 plt.legend()
 # show the plot
 plt.show()
+
+
 
 
